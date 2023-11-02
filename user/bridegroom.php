d324882e633c9d15d0fa1078cc4a933cea40ef17
Zivenlouis <zivenlouisuph@gmail.com>
2023-11-02 19:42:24 +0700

    ha

--- user/bridegroom.php
+++ user/bridegroom.php
@@ -1,3 +1,23 @@
-<?php
-    echo 'hello world';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style/general.css">
+    <link rel="stylesheet" href="assets/style/ziven.css">
+    <title>Alexander & Olivia Wedding Invitation</title>
+</head>
+<body>
+    <div class="container-inv">
+        <div class="golden-blur golden-blur-1"></div>
+        <div class="golden-blur golden-blur-2"></div>
+        <div class="golden-blur golden-blur-3"></div>
+        <div class="golden-blur golden-blur-4"></div>
+        <div class="leave-1 leave-a"></div>
+        <div class="leave-2 leave-a"></div>
+        <div class="leave-3 leave-b"></div>
+        <div class="leave-4 leave-b"></div>
+        abc
+    </div>
+</body>
+</html>
