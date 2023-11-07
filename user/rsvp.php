8371a63b39fd388f3e76a5e818e39cf3f3aa557a
irwantowjy <irwantowijaya0@gmail.com>
2023-11-07 17:27:13 +0700

    aa

---
+++ user/rsvp.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style/general.css">
+    <link rel="stylesheet" href="assets/style/ziven.css">    
+    <link rel="stylesheet" href="assets/style/rsvp.css">    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Belanosima&family=Montserrat:ital,wght@0,300;0,600;1,700&family=Redressed&display=swap" rel="stylesheet">
+    <title>RSVP</title>
+</head>
+<body>
+    <div class="container-inv">
+        <div class="golden-blur golden-blur-1"></div>
+        <div class="golden-blur golden-blur-2"></div>
+        <div class="golden-blur golden-blur-3"></div>
+        <div class="golden-blur golden-blur-4"></div>
+        <div class="leave-1 leave-a"></div>
+        <div class="leave-2 leave-a"></div>
+        <div class="container-rsvp">
+            <div class="rsvp-container-top">
+                <div class="flower">
+                    <img src="assets/images/rsvp/flower.png" alt="">
+                </div>
+                <div class="reservation-text">
+                    <h1>Reservation</h1>
+                    <img src="assets/images/rsvp/textFlower.png" alt="">
+                </div>
+            </div>
+            <div class="rsvp-container-down">
+                <div class="rsvp-down-left">
+                </div>
+                <div class="rsvp-down-right">
+                    <div class="header">
+                        Please help us confirming your attendance 
+                        to our wedding RSVP form:
+                    </div>
+                    <form action="">
+                        <label for="">Name</label>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div> 
+</body>
+</html>
