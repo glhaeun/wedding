124aca5f55035acc554c8c7ddbdbe1a51379a35b
Thiselvinaaa <elvinaxlim@gmail.com>
2023-11-16 18:01:46 +0700

    .

--- user/thankyou.php
+++ user/thankyou.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,43 +11,46 @@
     <link href="https://fonts.googleapis.com/css2?family=Enriqueta&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css">
 
-    
     <style>
-       .font-esthetic {
-    font-family: 'Ephesis', cursive !important;
-        }
+    .font-esthetic {
+        font-family: 'Ephesis', cursive !important;
+    }
 
     .font-arabic {
         font-family: 'Enriqueta', serif !important;
     }
 
     /* .yellow {
-        color: #ffd700 !important;
-    } */
-
+            color: #ffd700 !important;
+        } */
     </style>
 </head>
+
 <body>
     <section id="thankyou">
-       <footer>
-        <div class="container">
-            <div class="text-center">
+        <footer>
+            <div class="container">
+                <div class="text-center">
 
-                <p style="font-size: 0.9rem;" class="pt-2 pb-1 px-2" >
-                    Merupakan suatu kehormatan dan kebahagiaan bagi kami apabila, Bapak / Ibu / Saudara / i.
-                    Berkenan hadir untuk merayakan hari yang bahagia ini.
-                </p>
+                    <p style="font-size: 0.9rem;" class="pt-2 pb-1 px-2">
+                        Merupakan suatu kehormatan dan kebahagiaan bagi kami apabila, Bapak / Ibu / Saudara / i.
+                        Berkenan hadir untuk merayakan hari yang bahagia ini.
+                    </p>
 
-                <h1 class="font-esthetic yellow" >Terima kasih</h1>
-                <hr class="mt-3 mb-2">
+                    <img src="user/assets/images/elvina/Butterfly.png" alt="Kupu-kupu">
+                    <img src="user/assets/images/elvina/garisbelah.png" alt="Garis Belah">
 
+                    <h1 class="font-esthetic yellow">Terima kasih</h1>
+                    <hr class="mt-3 mb-2">
+
+                </div>
             </div>
-        </div>
-    </footer>
+        </footer>
     </section>
 </body>
+
 </html>
