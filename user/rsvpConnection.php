2215ab25b9ddb5ffd2390085822951e222d62d5e
glhaeun <113106028+glhaeun@users.noreply.github.com>
2023-11-21 18:35:50 +0700

    .

--- user/rsvpConnection.php
+++ user/rsvpConnection.php
@@ -5,8 +5,21 @@
     $kehadiran = $_POST['kehadiran'];
     $pesan = $_POST['pesan'];
 
+    if ($_SESSION['userEmail']) {
+        $email = $_SESSION['userEmail'];
+
+        $query = "UPDATE user SET attend='$kehadiran' WHERE email = '$email'";
+        if ($connect->query($query) === TRUE) {
+            echo "Data updated successfully";
+        }
+    }
+    
     $sql = "INSERT INTO message_rsvp (name, status, message) VALUES ('$nama', '$kehadiran', '$pesan')";
     if ($connect->query($sql) === TRUE) {
         echo "Data inserted successfully";
     }
+
+    
+
+
 ?>
